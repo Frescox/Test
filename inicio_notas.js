c6ef6b7a797a763dc63868d1218e34c31ac922f8
Frescox <luisangellopezgalicia@gmail.com>
2024-10-11 20:00:21 -0700

    sdafsd

--- inicio_notas.js
+++ inicio_notas.js
@@ -25,14 +25,3 @@ function loadText() {
         console.log("Texto cargado desde localStorage.");
     }
 }
-
-function removeText() {
-    localStorage.removeItem('noteContent'); // Elimina el contenido del localStorage
-    textInput.value = ""; // Limpia el textarea
-    console.log("Texto eliminado.");
-}
-
-function newNote() {
-    textInput.value = ""; // Limpia el campo de texto para una nueva nota
-    console.log("Nueva nota creada.");
-}
